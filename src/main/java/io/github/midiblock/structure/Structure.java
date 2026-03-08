db6a25251307b9c963d39210205a6f077f253ef2
penguin <penguin586970@gmail.com>
2026-03-07 22:40:48 -0500

    decrease structure complexity

--- src/main/java/io/github/midiblock/structure/Structure.java
+++ src/main/java/io/github/midiblock/structure/Structure.java
@@ -128,61 +128,105 @@ public class Structure {
         }
     }
 
-    private void buildPart(Channel channel, int x, int startZ) {
-        for (int i = 0; i < channel.getLength(); i++) {
-            Note note = channel.getNote(i);
-            int z = i * 2 + startZ;
-            if (x % 2 == 1) {
+
+    private void placeRepeaterRow(ArrayList<Channel> channels, int startX, int z, int delay) {
+        for (int x = 0; x < channels.size(); x++) {
+            int bx = x + startX;
+            if (bx % 2 == 1) {
                 add(new Block(
-                        new BlockPos(x, 0, z),
+                        new BlockPos(bx, 0, z),
                         Config.getFloor().defaultBlockState()
                 ));
                 add(new Block(
-                        new BlockPos(x, 1, z),
+                        new BlockPos(bx, 1, z),
                         Blocks.REPEATER.defaultBlockState()
                                 .setValue(RepeaterBlock.FACING, Direction.NORTH)
-                                .setValue(RepeaterBlock.DELAY, 1)
+                                .setValue(RepeaterBlock.DELAY, delay)
                                 .setValue(RepeaterBlock.LOCKED, false)
                                 .setValue(RepeaterBlock.POWERED, false)
                 ));
             }
+        }
+    }
+
+    private void placeNoteRow(ArrayList<Channel> channels, int startX, int tick, int z) {
+        for (int x = 0; x < channels.size(); x++) {
+            int bx = x + startX;
+            Note note = channels.get(x).getNote(tick);
             if (note != null) {
                 Instrument inst = note.getInstrument();
-
                 add(new Block(
-                        new BlockPos(x, 1, z + 1),
+                        new BlockPos(bx, 1, z),
                         Blocks.NOTE_BLOCK.defaultBlockState()
                                 .setValue(NoteBlock.INSTRUMENT, inst.getNoteBlockInstrument())
                                 .setValue(NoteBlock.POWERED, false)
                                 .setValue(NoteBlock.NOTE, note.getPitch())
                 ));
-
                 if (inst != Instrument.HARP) {
                     add(new Block(
-                            new BlockPos(x, 0, z + 1),
+                            new BlockPos(bx, 0, z),
                             inst.getBlock()
                     ));
                 }
-            }
-            else {
+            } else if (bx % 2 == 1) {
                 add(new Block(
-                        new BlockPos(x, 1, z + 1),
+                        new BlockPos(bx, 1, z),
                         Config.getConductive().defaultBlockState()
                 ));
             }
         }
     }
 
+    private boolean isRowEmpty(ArrayList<Channel> channels, int tick) {
+        for (Channel channel : channels) {
+            if (channel.getNote(tick) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getMaxLength(ArrayList<Channel> channels) {
+        int max = 0;
+        for (Channel channel : channels) {
+            max = Math.max(max, channel.getLength());
+        }
+        return max;
+    }
+
     public Structure fromSong(Song song) {
         int max = song.getMaxConcurrent();
         init(max - 1);
         ArrayList<Channel> parts = song.partition();
-        int x = 0;
-        if (parts.size() < 2) x++;
-        for (Channel channel : parts) {
-            buildPart(channel, x, 2);
-            x++;
+        int startX = 0;
+        if (parts.size() < 2) startX++;
+
+        int maxLength = getMaxLength(parts);
+        int z = 2;
+        int pendingDelay = 0;
+
+        for (int tick = 0; tick < maxLength; tick++) {
+            if (isRowEmpty(parts, tick)) {
+                pendingDelay++;
+                if (pendingDelay == 4) {
+                    placeRepeaterRow(parts, startX, z, 4);
+                    z++;
+                    pendingDelay = 0;
+                }
+            } else {
+                int totalDelay = pendingDelay + 1;
+                while (totalDelay > 0) {
+                    int d = Math.min(totalDelay, 4);
+                    placeRepeaterRow(parts, startX, z, d);
+                    z++;
+                    totalDelay -= d;
+                }
+                placeNoteRow(parts, startX, tick, z);
+                z++;
+                pendingDelay = 0;
+            }
         }
+
         return this;
     }
 }
